d0cd9c40a491da113726eb847fe88ad11be660aa
musulma24h <164721156+musulma24h@users.noreply.github.com>
2024-04-20 09:52:57 -0300

    Create ejemplo9_ unidad3.cpp

---
+++ Ejercicios sueltos/ejemplo9_ unidad3.cpp
@@ -0,0 +1,28 @@
+ ///ejemplo_9 
+ ///en el if para resumir el ELSE IF podemos hacer:
+ ///if(i==0 || n1 > max_num)
+ ///hacer un programa para ingresar 10 numeros, luego informar el maximo  
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, max_num;
+    for (int i=0; i < 10; i++)
+    {
+        cout << "escribir un numero" << endl;
+        cin>>n1;
+    
+        if (i==0)
+        {
+            max_num = n1;
+
+        }
+        else if(n1 > max_num) max_num = n1;
+
+    }
+
+    cout<< "NUMERO MAXIMO" << max_num << endl;
+    return 0;
+}
